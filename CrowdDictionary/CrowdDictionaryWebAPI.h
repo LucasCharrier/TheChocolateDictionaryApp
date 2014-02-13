f311358f8709c1eee0207c7ed4512b8b047f9926
lucaslivorno <lucharrier@gmail.com>
2014-02-13 17:08:22 -0600

    The Chocolate Dictionary - First Commit
    The first file of the application

---
+++ CrowdDictionary/CrowdDictionaryWebAPI.h
@@ -0,0 +1,13 @@
+//
+//  CrowdDictionaryWebAPI.h
+//  CrowdDictionary
+//
+//  Created by Lucas Charrier on 13/02/2014.
+//  Copyright (c) 2014 Lucas Charrier. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CrowdDictionaryWebAPI : NSObject
+
+@end
---
+++ CrowdDictionary/CrowdDictionaryWebAPI.m
@@ -0,0 +1,13 @@
+//
+//  CrowdDictionaryWebAPI.m
+//  CrowdDictionary
+//
+//  Created by Lucas Charrier on 13/02/2014.
+//  Copyright (c) 2014 Lucas Charrier. All rights reserved.
+//
+
+#import "CrowdDictionaryWebAPI.h"
+
+@implementation CrowdDictionaryWebAPI
+
+@end
